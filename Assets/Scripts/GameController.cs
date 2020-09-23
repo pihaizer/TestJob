7bfe590cb4b9a7475a8de5d3a5688d5ebcb7454c
Vladimir Malhin <56202294+pihaizer@users.noreply.github.com>
2020-09-23 20:01:13 +0300

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public Vector2Int numberOfStones;
+    public Vector2 firstStonePosition;
+    public Vector2 distanceBetweenStones;
+    public float columnOffset;
+    public GameObject stonePrefab;
+    void Start() {
+        PlaceStones();
+    }
+
+    private void PlaceStones() {
+        GameObject stonesObject = new GameObject("Stones");
+        for (int i = 0; i < numberOfStones.x; i++) {
+            for (int j = 0; j < numberOfStones.y; j++) {
+                Vector2 stonePosition = firstStonePosition +
+                    new Vector2(distanceBetweenStones.x * i + columnOffset * j, distanceBetweenStones.y * j);
+                Instantiate(stonePrefab, stonePosition, new Quaternion(), stonesObject.transform);
+            }
+        }
+    }
+}
